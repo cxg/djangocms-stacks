3603ffbf798c214bb3d46cc2109b138a2d08c359
Camillo Bruni <camillobruni@cxg.ch>
2015-05-12 17:32:33 +0200

    updated migrations to 1.7

--- stacks/migrations/0001_initial.py
+++ stacks/migrations/0001_initial.py
@@ -1,72 +1,40 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import cms.models.fields
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Stack'
-        db.create_table('stacks_stack', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(default='', max_length=255, blank=True)),
-            ('code', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255, blank=True)),
-            ('content', self.gf('django.db.models.fields.related.ForeignKey')(related_name='stacks_contents', null=True, to=orm['cms.Placeholder'])),
-        ))
-        db.send_create_signal('stacks', ['Stack'])
+    dependencies = [
+        ('cms', '0011_auto_20150419_1006'),
+    ]
 
-        # Adding model 'StackLink'
-        db.create_table('cmsplugin_stacklink', (
-            ('cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
-            ('stack', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['stacks.Stack'])),
-        ))
-        db.send_create_signal('stacks', ['StackLink'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Stack'
-        db.delete_table('stacks_stack')
-
-        # Deleting model 'StackLink'
-        db.delete_table('cmsplugin_stacklink')
-
-
-    models = {
-        'cms.cmsplugin': {
-            'Meta': {'object_name': 'CMSPlugin'},
-            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2012, 12, 10, 0, 0)'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
-            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.placeholder': {
-            'Meta': {'object_name': 'Placeholder'},
-            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'slot': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
-        },
-        'stacks.stack': {
-            'Meta': {'object_name': 'Stack'},
-            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255', 'blank': 'True'}),
-            'content': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'stacks_contents'", 'null': 'True', 'to': "orm['cms.Placeholder']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255', 'blank': 'True'})
-        },
-        'stacks.stacklink': {
-            'Meta': {'object_name': 'StackLink', 'db_table': "'cmsplugin_stacklink'", '_ormbases': ['cms.CMSPlugin']},
-            'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
-            'stack': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['stacks.Stack']"})
-        }
-    }
-
-    complete_apps = ['stacks']
+    operations = [
+        migrations.CreateModel(
+            name='Stack',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(default=b'', help_text='Descriptive name to identify this stack. Not displayed to users.', max_length=255, verbose_name='stack name', blank=True)),
+                ('code', models.CharField(help_text='To render the stack in templates.', unique=True, max_length=255, verbose_name='stack code', blank=True)),
+                ('content', cms.models.fields.PlaceholderField(related_name='stacks_contents', slotname='stack_content', editable=False, to='cms.Placeholder', null=True, verbose_name='stack content')),
+            ],
+            options={
+                'verbose_name': 'stack',
+                'verbose_name_plural': 'stacks',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='StackLink',
+            fields=[
+                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('stack', models.ForeignKey(verbose_name='stack', to='stacks.Stack')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('cms.cmsplugin',),
+        ),
+    ]
